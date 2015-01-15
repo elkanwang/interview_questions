8dbc95b1a5a46e0ec76c6ea1239feb2062d6da22
elkanwang <wangxiangkang@gmail.com>
2015-01-14 23:23:14 -0500

    Update two sum

--- leetcode/python/TwoSum.py
+++ leetcode/python/TwoSum.py
@@ -1,5 +1,15 @@
-def twoSum(num, target):
-	#
 
-num = {2,7,11,15}
-target = 9
+num = [3,2,4]
+target = 6
+
+def twoSum(num, target):
+	processed ={}
+	for i in range(0,len(num)):
+		if target-num[i] in processed:
+			return (processed.get(target-num[i])+1,i+1)
+		else:
+			processed[num[i]]=i
+
+	print processed
+
+print twoSum(num,target)
