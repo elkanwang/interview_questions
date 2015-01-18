291ec207b0710c9fe86d3760705a1d686533a0cb
elkanwang <wangxiangkang@gmail.com>
2015-01-18 15:26:23 -0500

    Word Break

---
+++ leetcode/python/wordBreak.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a boolean
+    def wordBreak(self, s, dict):
+        if s == '':
+            return True
+        checklist = [False]*(len(s)+1)
+        checklist[len(s)] = True
+        for i in range(len(s)-1,-1,-1):
+            for j in range(i,len(s)):
+                if s[i:j+1] in dict and checklist[j+1]==True:
+                    checklist[i]=True
+        return checklist[0]
