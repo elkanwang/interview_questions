73571b0301932b75778631489e0d70f3a398e0ad
elkanwang <wangxiangkang@gmail.com>
2015-06-22 14:09:56 -0400

    edited arrays.py

--- crackingTheCodingInterview/arrays.py
+++ crackingTheCodingInterview/arrays.py
@@ -1,3 +1,5 @@
+import unittest
+import re
 
 # solutions for Chapter 1: Arrays
 
@@ -7,16 +9,14 @@ class Solution():
 
 # q1. determine if an array has all unique characters
 
-    def uniqueChar(string):
-        strLen = len(string)
-        if strLen == 0:
-            return True
+    def uniqueChar(self, string):
         pool = {}
         for char in string:
             if char in pool.keys():
                 return False
             else:
                 pool[char] = 1
+        return True
 
 # q2. implement void reverse(char* str)
 
@@ -30,10 +30,71 @@ class Solution():
 #        return newStr
 
 # q3. given two strings, determine if one is permutation of the other
+# this is the same question as "anagram"
 
-    def isPermutation(str1, str2):
-        str1.sort()
-        str2.sort()
+    def isPermutation(self, str1, str2):
+        str1 = ''.join(sorted(str1))
+        str2 = ''.join(sorted(str2))
         return str1 == str2
 
+    def isPermutation2(self, str1, str2):
+        pool = {}
+        for char in str1:
+            pool[char] = 1 if char not in pool.keys() else pool[char] + 1
+        for char in str2:
+            if char not in pool.keys():
+                return False
+            else:
+                pool[char] -= 1
+        for value in pool.values():
+            if value != 0:
+                return False
+        return True
+
 # q4. replace whitespace with %20, implement in-place solution
+
+    def replaceSpace(self, s):
+        s = re.sub(r' ', '%20', s)
+        return s
+
+    def replaceSpace2(self, s):
+        pass
+
+# q5.
+
+# q6
+
+# q7
+
+# q8
+
+# q9
+
+# q10.
+
+
+class Test(unittest.TestCase):
+
+    def testUniqueChar(self):
+        self.assertEqual(Solution().uniqueChar('abc'), True)
+
+    def testUniqueChar2(self):
+        self.assertEqual(Solution().uniqueChar('aaa'), False)
+
+    def test1(self):
+        self.assertEqual(Solution().isPermutation('aaa', 'bbb'), False)
+
+    def test2(self):
+        self.assertEqual(Solution().isPermutation('abcdef', 'fecbad'), True)
+
+    def test3(self):
+        self.assertEqual(Solution().isPermutation2('aaa', 'bbb'), False)
+
+    def test4(self):
+        self.assertEqual(Solution().isPermutation2('abcdef', 'fecbad'), True)
+
+    def test5(self):
+        self.assertEqual(Solution().replaceSpace('a a'), 'a%20a')
+
+if __name__ == '__main__':
+    unittest.main()
