0ee4f712c1ad40f0c5ae1c16c3fd504166c7c5c9
Xiangkang Wang <xgwang@xgwang-mn1.linkedin.biz>
2016-06-20 16:27:16 -0700

    changed tab to spaces

--- leetcode/python/152_maxProductSubarry.py
+++ leetcode/python/152_maxProductSubarry.py
@@ -5,8 +5,9 @@ class Solution:
 			return 0
 		front_max = front_min = max_product = nums[0]
 		for i in range(1, n):
-		    front_max, front_min = max(nums[i], nums[i]*front_max, nums[i]*front_min), min(nums[i], nums[i]*front_max, nums[i]*front_min)
-		    max_product = max(max_product, front_min, front_max)
+			front_max, front_min = max(nums[i], nums[i]*front_max, nums[i]*front_min), \
+								   min(nums[i], nums[i]*front_max, nums[i]*front_min)
+			max_product = max(max_product, front_min, front_max)
 		return max_product
 
 sol = Solution()
