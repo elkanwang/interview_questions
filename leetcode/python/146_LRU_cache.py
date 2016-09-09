a6040bf3307fd0df576f4857947b93392896f58e
elkanwang <wangxiangkang@gmail.com>
2016-09-09 10:29:41 -0400

    google qns

--- leetcode/python/146_LRU_cache.py
+++ leetcode/python/146_LRU_cache.py
@@ -1,9 +1,52 @@
 class Node(object):
 
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+    def __repr__(self):
+        return 'node: ' +  str(self.key) + str(self.value)
+
+
+class DoublyLinkedList(object):
+
     def __init__(self):
-        self.value = None
-        self.left = None
-        self.right = None
+        self.head = None
+        self.tail = None
+
+    def remove(self, node):
+        if node.prev is not None:
+            node.prev.next = node.next
+        else:
+            self.head = node.next
+        if node.next is not None:
+            node.next.prev = node.prev
+        else:
+            self.tail = node.prev
+        node.next = None
+        node.prev = None
+
+    def insert(self, new_node):
+        new_node.prev = self.tail
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def append(self, node_value):
+        new_node = Node(node_value)
+        self.insert(new_node)
+
+    def __repr__(self):
+        cur_node = self.head
+        ll_str = ''
+        while cur_node:
+            ll_str += '{0} -> '.format(cur_node.value)
+            cur_node = cur_node.next
+        return ll_str + 'None'
 
 
 class LRUCache(object):
@@ -12,8 +55,7 @@ class LRUCache(object):
         :type capacity: int
         """
         self.cache = {}
-        self.linked_list_head = Node()
-        self.linked_list_tail = Node()
+        self.ll = DoublyLinkedList()
         self.capacity = capacity
 
     def get(self, key):
@@ -21,10 +63,12 @@ class LRUCache(object):
         :rtype: int
         """
         if key in self.cache:
-            pass
+            node = self.cache[key]
+            self.ll.remove(node)
+            self.ll.insert(node)
+            return node.value
         else:
             return -1
-
 
     def set(self, key, value):
         """
@@ -34,28 +78,40 @@ class LRUCache(object):
         """
         if key in self.cache:
             node = self.cache[key]
-            node_left = node.left
-            node_right = node.right
-            node_left.right = node_right
-            node_right.left = node_left
-            res_value = node.value[1]
-            # insert back
-            node.value = (key, value)
-            node.right = self.linked_list_head
-            self.linked_list_tail.left = node
-            return res_value
-        elif len(self.cache) < self.capacity:
-            node = Node()
-            node.value = (key, value)
-            node.right = self.linked_list_head
-            self.linked_list_tail.left = node
-            self.cache[key] = node
+            node.value = value
+            self.ll.remove(node)
+            self.ll.insert(node)
         else:
-            # remove right most
-            node_to_delete = self.linked_list_tail
-            key_to_delete = node_to_delete.value[0]
-            del self.cache[key_to_delete]
-            self.linked_list_tail = node_to_delete.left
-            node_to_delete.left.right = None
+            node = Node(key, value)
+            self.cache[key] = node
+            if len(self.cache) <= self.capacity:
+                self.ll.insert(node)
+            else:
+                # print key, value, self.cache, self.ll
+                self.cache.pop(self.ll.head.key)
+                self.ll.remove(self.ll.head)
+                self.ll.insert(node)
 
 
+# cache = LRUCache(1)
+# # print cache.get(3)
+# cache.set(2, 1)
+# print cache.get(2)
+# cache.set(3, 2)
+# # cache.set(1992, 1992)
+# print cache.get(2)
+# # print cache.ll
+# # cache.set(1993, 1993)
+# # print cache.ll, cache.cache
+# print cache.get(3)
+
+
+cache = LRUCache(2)
+cache.set(2, 1)
+cache.set(1, 1)
+print cache.ll
+print cache.get(2)
+print cache.ll
+cache.set(4, 1)
+print cache.get(1)
+print cache.get(2)
---
+++ leetcode/python/308_range_sum_query_2d_mutable.py
@@ -0,0 +1,32 @@
+class NumMatrix(object):
+    def __init__(self, matrix):
+        """
+        initialize your data structure here.
+        :type matrix: List[List[int]]
+        """
+
+    def update(self, row, col, val):
+        """
+        update the element at matrix[row,col] to val.
+        :type row: int
+        :type col: int
+        :type val: int
+        :rtype: void
+        """
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        sum of elements matrix[(row1,col1)..(row2,col2)], inclusive.
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+
+
+        # Your NumMatrix object will be instantiated and called as such:
+        # numMatrix = NumMatrix(matrix)
+        # numMatrix.sumRegion(0, 1, 2, 3)
+        # numMatrix.update(1, 1, 10)
+        # numMatrix.sumRegion(1, 2, 3, 4)
